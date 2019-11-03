aefc82ff1ea464ecb70ef02eaa2e8188c111ef5a
Ruslan Shuranov <ruslanshuranov@gmail.com>
2019-11-03 06:26:10 +0200

    Adding new todo. Controlled form object

--- reminds/src/components/AddTodoPanel/AddTodoPanel.js
+++ reminds/src/components/AddTodoPanel/AddTodoPanel.js
@@ -4,18 +4,34 @@ import './AddTodoPanel.css'
 
 export default class AddTodoPanel extends React.Component {
 
+    state = {
+        label: ''
+    }
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.props.addNewTodo(this.state.label)
+        this.setState({
+            label: ''
+        })
+    }
+
+
     render() {
 
-        const { addNewTodo } = this.props
-
         return (
-            <div className='add-todo-panel input-group'>
+            <form className='add-todo-panel input-group' onSubmit={this.onSubmit}>
                 <input type="text" className="form-control" placeholder="Type new todo"
-                       aria-label="Add new todo" aria-describedby="basic-addon2"/>
+                       aria-label="Add new todo" aria-describedby="basic-addon2" onChange={this.onLabelChange} value={this.state.label}/>
                 <div className="input-group-append">
-                    <button className="btn btn-outline-success" type="button" onClick={() => addNewTodo('hello')}><i className="fa fa-plus"/> Add</button>
+                    <button className="btn btn-outline-success"><i className="fa fa-plus"/> Add</button>
                 </div>
-            </div>
+            </form>
         )
     }
 }
