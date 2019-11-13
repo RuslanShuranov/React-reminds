e8f235c7fab1b2ee0e6e9b804e2bbc68e949b697
Ruslan Shuranov <ruslanshuranov@gmail.com>
2019-11-13 10:23:13 +0200

    Added Error indicator

--- stardb/src/components/RandomPlanet/RandomPlanet.js
+++ stardb/src/components/RandomPlanet/RandomPlanet.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import './RandomPlanet.css';
 import SwapiService from '../../services/swapi-service';
 import Spiner from '../Spinner';
+import ErrorIndicator from '../ErrorIndicator';
 
 export default class RandomPlanet extends Component {
 
@@ -21,24 +22,39 @@ export default class RandomPlanet extends Component {
     onPlanetLoaded = (planet) => {
         this.setState({
             planet,
+            loading: false,
+            error: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true,
             loading: false
         })
     }
 
     updatePlanet() {
-        const id = Math.floor(Math.random() * 25) + 2
+        // const id = Math.floor(Math.random() * 25) + 2
+        const id =12000
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoaded)
+            .catch(this.onError)
     }
 
     render() {
-        const {planet, loading} = this.state
+        const {planet, loading, error} = this.state
 
+        const hasData = !(loading || error)
+
+        const errorIndicator = error ? <ErrorIndicator/> : null
         const spinner = loading ? <Spiner/> : null
-        const content = !loading ? <PlanetView planet={planet}/> : null
+        const content = hasData && !error ? <PlanetView planet={planet}/> : null
+
         return (
             <div className="random-planet jumbotron rounded">
+                {errorIndicator}
                 {spinner}
                 {content}
             </div>
