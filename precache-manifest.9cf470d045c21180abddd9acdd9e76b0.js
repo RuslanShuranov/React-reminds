3dedbdbaa7a144492877abcc86025bc5cb64d046
Ruslan Shuranov <ruslanshuranov@gmail.com>
2019-11-27 15:39:04 +0200

    Updates

---
+++ precache-manifest.9cf470d045c21180abddd9acdd9e76b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d99729bd924e72e6f5de1201e609de",
+    "url": "/react-reminds/index.html"
+  },
+  {
+    "revision": "fdc069dcc818c2e89586",
+    "url": "/react-reminds/static/css/main.7c07a1e9.chunk.css"
+  },
+  {
+    "revision": "a6c454a6ab281692b999",
+    "url": "/react-reminds/static/js/2.34fc7943.chunk.js"
+  },
+  {
+    "revision": "fdc069dcc818c2e89586",
+    "url": "/react-reminds/static/js/main.cb1ac89d.chunk.js"
+  },
+  {
+    "revision": "f28209a0cded774c8fc9",
+    "url": "/react-reminds/static/js/runtime-main.ff0dfecd.js"
+  }
+]);
